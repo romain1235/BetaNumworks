2b5d6e9d7f1a6fe1eef83a16b6c2bc3fba40279b
Yaya-Cout <yaya.cout@ik.me>
2025-11-26 22:11:08 +0100

    [bootloader] Mark Epsilon 24.10.0 as safe

--- bootloader/boot.cpp
+++ bootloader/boot.cpp
@@ -105,7 +105,7 @@ void Boot::bootSlot(Bootloader::Slot s) {
   if (!s.userlandHeader()->isOmega() && !s.userlandHeader()->isUpsilon()) {
     // We are trying to boot epsilon, so we check the version and show a warning if needed
     const char * version = s.userlandHeader()->version();
-    const char * min = "24.4.1";
+    const char * min = "24.10.1";
     int versionSum = Utility::versionSum(version, strlen(version));
     int minimalVersionTrigger = Utility::versionSum(min, strlen(min));
     if (versionSum >= minimalVersionTrigger) {
